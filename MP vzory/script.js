3a6d5708fdd800e762d0cc14f2f349353e9fb8b8
StudioVenda <vaclav.sontag@gmail.com>
2025-03-15 04:23:43 +0100

    Konečná MP aktualizace

---
+++ MP vzory/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let content = document.getElementById("content");
+  content.innerHTML = "<div>Obsah přidán dynamicky pomocí JavaScriptu</div>";
+});
